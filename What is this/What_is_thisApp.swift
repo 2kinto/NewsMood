9ed68cc189789882a223e2add08ee8f75fee0fc4
JT-Phantasy <15558188801jt@gmail.com>
2021-01-30 03:33:53 +0900

    Initial Commit

---
+++ What is this/What_is_thisApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_is_thisApp.swift
+//  What is this
+//
+//  Created by Phantasy on 2021/1/30.
+//
+
+import SwiftUI
+
+@main
+struct What_is_thisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
